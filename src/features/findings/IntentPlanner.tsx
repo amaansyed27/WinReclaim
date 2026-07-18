29ca25bc04e3393a0d8e7c4fa55b861e99e5ccfd
Amaan Syed <amaansyed27@gmail.com>
2026-07-18 16:07:48 +0530

    Keep React type imports explicit

--- src/features/findings/IntentPlanner.tsx
+++ src/features/findings/IntentPlanner.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from "react";
+import { useState } from "react";
+import type { FormEvent } from "react";
 import { formatBytes } from "../../lib/format";
 import type { AiStatus } from "../../types";
 import "./IntentPlanner.css";
