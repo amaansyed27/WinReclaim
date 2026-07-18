9eb2f0d9b1fabeda5a2963dd12eefa5096e59daa
Amaan Syed <amaansyed27@gmail.com>
2026-07-18 15:45:35 +0530

    Keep long filesystem work off the UI thread

--- src-tauri/src/storage/mod.rs
+++ src-tauri/src/storage/mod.rs
@@ -1,12 +1,26 @@
 use crate::domain::{CleanupPlan, CleanupReceipt, ScanReport};
 use std::collections::HashMap;
-use std::sync::{atomic::AtomicBool, Mutex};
+use std::sync::{
+    atomic::AtomicBool,
+    Arc, Mutex,
+};
 use uuid::Uuid;
 
-#[derive(Default)]
+#[derive(Clone)]
 pub struct AppState {
-    pub cancel_scan: AtomicBool,
-    pub latest_scan: Mutex<Option<ScanReport>>,
-    pub plans: Mutex<HashMap<Uuid, CleanupPlan>>,
-    pub receipts: Mutex<Vec<CleanupReceipt>>,
+    pub cancel_scan: Arc<AtomicBool>,
+    pub latest_scan: Arc<Mutex<Option<ScanReport>>>,
+    pub plans: Arc<Mutex<HashMap<Uuid, CleanupPlan>>>,
+    pub receipts: Arc<Mutex<Vec<CleanupReceipt>>>,
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        Self {
+            cancel_scan: Arc::new(AtomicBool::new(false)),
+            latest_scan: Arc::new(Mutex::new(None)),
+            plans: Arc::new(Mutex::new(HashMap::new())),
+            receipts: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
 }
