8b3fe23a5ec4f4a852516ec932bf7ad1b6b714ef
Amaan Syed <amaansyed27@gmail.com>
2026-07-19 10:37:29 +0530

    Expose dynamic scanner module

--- src-tauri/src/scanner/mod.rs
+++ src-tauri/src/scanner/mod.rs
@@ -1,5 +1,7 @@
+mod discovery;
 mod profile;
 mod sizing;
 
+pub use discovery::{discover_unknown_directories, DiscoveryOptions, DiscoveryResult};
 pub use profile::scan_profile;
 pub use sizing::directory_size;
