ac726db5052250709a80e3c1c5d04777e42271ad
Amaan Syed <amaansyed27@gmail.com>
2026-07-20 18:08:45 +0530

    Load simplified interface styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import { App } from "./App";
 import "./styles.css";
 import "./desktop-tuning.css";
 import "./time-machine.css";
+import "./simple-ui.css";
 
-// Keep the desktop shell and storage-intelligence styles loaded as one application surface.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
