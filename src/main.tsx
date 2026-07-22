286e0fc1e6545da3a72786135a745ca0fa86fd1a
Amaan Syed <amaansyed27@gmail.com>
2026-07-22 11:27:09 +0530

    style: load review workspace tabs

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./time-machine.css";
 import "./simple-ui.css";
 import "./multi-drive.css";
 import "./findings-refresh.css";
+import "./review-tabs.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
